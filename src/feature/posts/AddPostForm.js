a560fd3db2e71cd0edb72545e9fedaa114d591ce
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-16 20:53:01 +0530

    making post request async

--- src/feature/posts/AddPostForm.js
+++ src/feature/posts/AddPostForm.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { postAdded } from "./postSlice";
+import { addNewPost /* postAdded */ } from "./postSlice";
 import { selectAllUsers } from "../users/userSlice";
 const AddPostForm = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [userId, setUserId] = useState("");
+  const [addRequestStatus, setAddRequestStatus] = useState("idle");
   const dispatch = useDispatch();
 
   const users = useSelector(selectAllUsers);
@@ -14,16 +15,27 @@ const AddPostForm = () => {
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
   const onAuthorChanged = (e) => setUserId(e.target.value);
+
+  /* const canSave = Boolean(title) && Boolean(content) && Boolean(userId); */
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === "idle";
+
   const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(postAdded(title, content, userId));
-      setUserId("");
-      setTitle("");
-      setContent("");
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
+        /*  dispatch(postAdded(title, content, userId)); */
+        setUserId("");
+        setTitle("");
+        setContent("");
+      } catch (e) {
+        console.error("Failed to save post", e);
+      } finally {
+        setAddRequestStatus("idle");
+      }
     }
   };
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
   const userOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
