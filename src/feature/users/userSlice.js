5b4ea6e0946e94c0ddfd962b5e971cb446b548de
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-16 20:35:17 +0530

    users api async is attached

--- src/feature/users/userSlice.js
+++ src/feature/users/userSlice.js
@@ -1,15 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  { id: "0", name: "alice alan" },
-  { id: "1", name: "Parneeta " },
-  { id: "2", name: "Vikal" },
-];
+const USER_URL = "https://jsonplaceholder.typicode.com/users";
+
+const initialState = [];
+
+export const fetchUser = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await axios.get(USER_URL);
+  return response.data;
+});
 
 export const userSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUser.fulfilled, (state, action) => {
+      return action.payload; //replace complete state
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { fetchUser } from "./feature/users/userSlice";
+
+store.dispatch(fetchUser());
 
 ReactDOM.render(
   <React.StrictMode>
